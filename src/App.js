a44d3a927cbaaa6f8065e65cc9d59df8ee65e0b1
Lenier G. Fontaine <lenierfontaine@gmail.com>
2023-11-18 05:10:41 -0500

    Add basic UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  return (
+    <div>
+      <h1>Computer Vision</h1>
+      <label>Insert URL or type prompt:</label>
+      <br/>
+      <input className="w-100" type="text" placeholder="Enter URL to analyze or textual prompt to generate an image" />
+      <br/>
+      <button>Analyze</button>
+      <button className="pl mt">Generate</button>
+    </div>
+  );
 }
 
 export default App;
